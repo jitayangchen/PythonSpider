4668215590ae2a51cb238ade33ac8efaaa596138
Yangchen <jitayangchen@163.com>
2016-11-06 22:50:06 +0800

    mmmm

--- test/Hello.py
+++ test/Hello.py
@@ -26,17 +26,34 @@ import urllib2
 #
 # print(re.compile(r"/view/\d+\.htm"))
 
-root_url = "http://pic.14bobo.com/desimgs/201510/20151030410261026.jpg"
+root_url = "http://pic.d741.com/d4/3030/303001-2.jpg"
 # arr = root_url.split('/')
 #
 # print(arr[len(arr) - 1])
 
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-          'Accept': 'text/html;q=0.9,*/*;q=0.8',
-          'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-          'Accept-Encoding': 'gzip',
-          'Connection': 'close',
-          'Referer': 'pic.14bobo.com'}
+# Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+# Accept-Encoding:gzip, deflate, sdch
+# Accept-Language:zh-CN,zh;q=0.8,en;q=0.6
+# Cache-Control:max-age=0
+# Connection:keep-alive
+# Cookie:__cfduid=d399a5506c1d2fb6aba33dcc7feaea39c1475839310
+# Host:pic.d741.com
+# If-Modified-Since:Tue, 19 Apr 2016 21:57:50 GMT
+# Referer:http://www.482cao.com/html/article/index141670.html
+# Upgrade-Insecure-Requests:1
+# User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36
+
+header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+          'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
+          'Accept-Encoding': 'gzip, deflate, sdch',
+          'Cache-Control': 'max-age=0',
+          'If-Modified-Since': 'Tue, 19 Apr 2016 21:57:50 GMT',
+          'Cookie': '__cfduid=d399a5506c1d2fb6aba33dcc7feaea39c1475839310',
+          'Host': 'pic.d741.com',
+          'Upgrade-Insecure-Requests': '1',
+          'Connection': 'keep-alive',
+          'Referer': 'http://www.482cao.com/html/article/index141670.html'}
 
 print("---------- download stat ----------")
 save_path = os.getcwd() + '/imgs/'

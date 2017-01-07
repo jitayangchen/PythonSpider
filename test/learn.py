625b47ff2f10659ef61f7c29f1a36514251506b1
Yangchen <jitayangchen@163.com>
2017-01-07 22:42:38 +0800

    new

--- test/learn.py
+++ test/learn.py
@@ -1,3 +1,10 @@
 import random
+import os
 
-print random.randint(1, 10) / 10.0
+# print random.randint(1, 10) / 10.0
+
+path = 'imgs2'
+isExists = os.path.exists(path)
+print isExists
+if not isExists:
+    os.mkdir(path)
--- test/spider_lalala.py
+++ test/spider_lalala.py
@@ -5,8 +5,6 @@ import os
 import random
 import urlparse
 
-root_url = "http://www.299gan.com/html/pic/as/Heyzo01041700426P/"
-
 header = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -29,9 +27,7 @@ header = {
 #     print urlparse.urljoin(root_url, new_url)
 
 
-def download(img_url):
-    save_path = '/mnt/SanDisk/imgs2/'
-    # save_path = os.getcwd() + '/imgs/'
+def download(img_url, save_path):
     arr = img_url.split('/')
 
     result = requests.get(img_url, None, headers=header)
@@ -41,19 +37,33 @@ def download(img_url):
         f.flush()
         f.close()
 
-count = 0
-countUrl = 0
-for i in range(100):
-    result = requests.get(root_url, None, headers=header)
-    if result.status_code == 200:
-        html = etree.HTML(result.text)
-        new_url = html.xpath('//*[@class="next_pre"]/span[2]/a/@href')[0]
-        root_url = urlparse.urljoin(root_url, new_url)
-        imgUrls = html.xpath('//*[@class="news"]/img/@src')
-        countUrl += 1
-        print 'Url page === ', countUrl
-        for imgUrl in imgUrls:
-            download(imgUrl)
-            count += 1
-            print 'count === ', count
-            time.sleep(random.randint(1, 10) / 10.0)
+
+def main():
+    root_url = "http://www.299gan.com/html/pic/as/Heyzo01041700426P/"
+
+    # save_path = '/mnt/SanDisk/imgs2/'
+    save_path = os.getcwd() + '/imgs/'
+    is_exists = os.path.exists(save_path)
+    print is_exists
+    if not is_exists:
+        os.mkdir(save_path)
+
+    count = 0
+    count_url = 0
+    for i in range(3):
+        result = requests.get(root_url, None, headers=header)
+        if result.status_code == 200:
+            html = etree.HTML(result.text)
+            new_url = html.xpath('//*[@class="next_pre"]/span[2]/a/@href')[0]
+            root_url = urlparse.urljoin(root_url, new_url)
+            img_urls = html.xpath('//*[@class="news"]/img/@src')
+            count_url += 1
+            print 'Url page === ', count_url
+            for imgUrl in img_urls:
+                download(imgUrl, save_path)
+                count += 1
+                print 'count === ', count
+                time.sleep(random.randint(1, 10) / 10.0)
+
+
+main()

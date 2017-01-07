6da4660405d6832ba1c1e2b6e44025df258c3b9a
Yangchen <jitayangchen@163.com>
2017-01-07 13:37:10 +0800

    dddd

--- test/spider.py
+++ test/spider.py
@@ -4,16 +4,18 @@ from lxml import etree
 import os
 import random
 
-root_url = "http://www.mzitu.com/83092"
+root_url = "http://www.mzitu.com/82667"
 
-header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
-          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-          'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
-          'Accept-Encoding': 'gzip, deflate, sdch',
-          'Cache-Control': 'max-age=0',
-          # 'Host': 'pic.d741.com',
-          'Upgrade-Insecure-Requests': '1',
-          'Connection': 'keep-alive'}
+header = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Cache-Control': 'max-age=0',
+    # 'Host': 'pic.d741.com',
+    'Upgrade-Insecure-Requests': '1',
+    'Connection': 'keep-alive'}
+
 
 # result = requests.get(root_url, None, headers=header)
 # if result.status_code == 200:
@@ -24,7 +26,7 @@ header = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWe
 
 def download(img_url):
     print("---------- download stat ----------")
-    save_path = os.getcwd() + '/imgs/'
+    save_path = '/mnt/SanDisk/imgs/'
     arr = img_url.split('/')
 
     result = requests.get(img_url, None, headers=header)
@@ -35,12 +37,14 @@ def download(img_url):
         f.close()
     print("---------- download complete ----------")
 
-
-for i in range(55):
+count = 0
+for i in range(66):
     result = requests.get(root_url, None, headers=header)
     if result.status_code == 200:
         html = etree.HTML(result.text)
         root_url = html.xpath('//*[@class="pagenavi"]/a[last()]/@href')[0]
         imgUrl = html.xpath('//*[@class="main-image"]/p/a/img/@src')[0]
         download(imgUrl)
+        count += 1
+        print 'count === ', count
         time.sleep(random.randint(1, 2))
